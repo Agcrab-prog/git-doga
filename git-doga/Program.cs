b4f03bdf027d5f2106d25d8ca224479aca3f0733
Agcrab-prog <magda.agoston123@gmail.com>
2023-12-18 12:25:22 +0100

    Az egész kód egyben feltoltesre kerult. A kod egyszeruen mukodik a feldatban megadott parameterekkel dolgoztam. A kód jó és működőképes. A feldat elkészült

--- git-doga/Program.cs
+++ git-doga/Program.cs
@@ -1,15 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace git_doga
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        List<int> szamok = new List<int>();
+
+        Console.WriteLine("Adj meg számokat, 0 végjelig:");
+        int szam;
+        do
         {
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out szam))
+            {
+                szamok.Add(szam);
+
+                Console.WriteLine($"Beirt szamok: {string.Join(", ", szamok)}");
+            }
+            else
+            {
+                Console.WriteLine("Ervenytelen szam, probald ujra!");
+            }
+        } while (szam != 0);
+
+        // 4. Feladat: Minimum kiválasztás és kiírás
+        int minSzam = szamok.Count > 0 ? szamok[0] : 0;
+        foreach (var elem in szamok)
+        {
+            if (elem < minSzam)
+            {
+                minSzam = elem;
+            }
         }
+        Console.WriteLine($"A legkisebb szam: {minSzam}");
+
+        // 5. Feladat: Maximum kiválasztás és kiírás
+        int maxSzam = szamok.Count > 0 ? szamok[0] : 0;
+        foreach (var elem in szamok)
+        {
+            if (elem > maxSzam)
+            {
+                maxSzam = elem;
+            }
+        }
+        Console.WriteLine($"A legnagyobb szam: {maxSzam}");
+
+        Console.ReadKey();
     }
 }
